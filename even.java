2f6468025363aa977b842bda81b68ada7678202f
nandhu321 <48095407+nandhu321@users.noreply.github.com>
2019-03-20 14:04:14 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class even
+{
+public static void main(String args[])
+{int l;
+ Scanner sc=new Scanner(System.in);
+ l=sc.nextInt();
+ if(l>0)
+{
+if(l%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+else 
+{
+System.out.println("invalid");
+}
+}
+}
