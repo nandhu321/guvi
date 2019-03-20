b81cfd2e174ef9eabcecc36e7a616dc57e9bd6a6
nandhu321 <48095407+nandhu321@users.noreply.github.com>
2019-03-20 13:49:28 +0530

    Create positiive.java

---
+++ positiive.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class sai
+{
+public static void main(String args[])
+{
+ int l;
+ Scanner sc=new Scanner(System.in);
+ l=sc.nextInt();
+ if(l<0)
+{
+System.out.println("Negative");
+}
+else if(l>0)
+{
+System.out.println("Positive");
+}
+else
+{
+System.out.println("Zero");
+}
+}
+}
