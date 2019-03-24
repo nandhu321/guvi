15e4285f7378a148076ec0466485260e178c08de
nandhu321 <48095407+nandhu321@users.noreply.github.com>
2019-03-24 19:02:03 +0530

    Create repeat1.py

---
+++ repeat1.py
@@ -0,0 +1,32 @@
+n=int(input())
+a=[]
+b=[]
+for i in range(n):
+    x=list(input())
+    a.append(x)
+m=len(a[0])
+n=a[0]
+
+for i in range(1,len(a)):
+    if m<len(a[i]):
+        m=m
+    else:
+        m=len(a[i])
+        n=a[i]
+
+for i in range(m):
+    k=j=0
+    while k<len(a):
+        if n[i]!=a[k][i]:
+            j=-1
+            break
+        else:
+            j=1
+            k=k+1
+    if j==1:
+        b.append(n[i])
+    elif j==-1:
+        break
+str=""
+b=str.join(b)
+print(b)
