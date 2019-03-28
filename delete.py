739150041ebcc74344ab1b06bc51c15d2b843d38
nandhu321 <48095407+nandhu321@users.noreply.github.com>
2019-03-28 18:34:28 +0530

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,9 +1,8 @@
-n,k = map(int,input().split())
-a= []
-s = str(n)
-for i in range(k):
-    first = i
-    last = len(s)-k+i
-    x = s[first:last]
-    a.append(x)
+n,k=map(str,input().split())
+g=len(n)-int(k)
+a=[]
+for i in range(len(n)):
+    f=n[i:i+g]
+    if len(f)==g:
+        a.append(int(f))
 print(min(a))
