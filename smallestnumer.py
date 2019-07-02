bb650235f07d4f6a5921842c736a6ba14e0250d2
nandhu321 <48095407+nandhu321@users.noreply.github.com>
2019-07-02 18:20:51 +0530

    Update and rename smallestnum.py to smallestnumer.py

--- smallestnumer.py
+++ smallestnumer.py
@@ -2,8 +2,11 @@ m,n=map(int,input().split())
 if(m>n):
     if(m%n==0):
         print(n)
-else:
+elif(n>m):
     if(n%m==0):
         print(m)
+else:
+    print(m)
 if(m%n!=0 and n%m!=0):
     print(m*n)
+
