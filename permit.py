3754c538500711408f68de97e6470d58b3a03bca
nandhu321 <48095407+nandhu321@users.noreply.github.com>
2019-07-02 11:53:07 +0530

    Create permit.py

---
+++ permit.py
@@ -0,0 +1,25 @@
+def generatePermutation(string,start,end):  
+    current = 0;  
+    
+    if(start == end-1):  
+        print(string);  
+    else:   
+        for current in range(start,end):  
+  
+        
+            x = list(string);  
+            temp = x[start];  
+            x[start] = x[current];  
+            x[current] = temp;  
+  
+     
+  
+            generatePermutation("".join(x),start+1,end);  
+           
+            temp = x[start];  
+            x[start] = x[current];  
+            x[current] = temp;  
+  
+str = input()
+n = len(str);  
+generatePermutation(str,0,n);  
