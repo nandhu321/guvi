a255a7dcdf398e9f7ad8bb0784d7bfc186ed5fa1
nandhu321 <48095407+nandhu321@users.noreply.github.com>
2019-07-03 13:49:21 +0530

    Create gcd.py

---
+++ gcd.py
@@ -0,0 +1,11 @@
+import math
+n,m=map(int,input().split())
+c=[]
+b=list(map(int,input().split()))
+for i in range(0,m):
+    l,h=map(int,input().split())
+    c.append([l,h])
+for i in c:
+    d=i[0]-1
+    e=i[1]-1
+    print(math.gcd(b4[d],b4[e]))
