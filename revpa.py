08aa92642e656ccd15c53998eb62132c3768402a
nandhu321 <48095407+nandhu321@users.noreply.github.com>
2019-07-03 13:17:19 +0530

    Update revpa.py

--- revpa.py
+++ revpa.py
@@ -9,15 +9,13 @@ for i in range(0,l):
     c=c/10
 j=sum1
 if(sum1<10):
-    print("yes")
+    print("YES")
 else:
     while (sum1!=0):
         g=int(sum1)%10
         rev=rev*10+g
         sum1=sum1//10
-if(rev==j):
-     print("YES")
-else:
-     print("NO")
-   
-    
+    if(rev==j):
+        print("YES")
+    else:
+        print("NO")
