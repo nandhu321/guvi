8a83f802050272d0dacf2f9632c23aceef7bec13
nandhu321 <48095407+nandhu321@users.noreply.github.com>
2019-07-04 13:40:47 +0530

    Create balancing.py

---
+++ balancing.py
@@ -0,0 +1,12 @@
+c=input()
+l1=len(c)
+count=0
+for i in range(0,l1):
+    if(c[i]=="("):
+        count=count+1
+    elif(c[i]==")"):
+        count=count-1
+if(count==0):
+    print("yes")
+else:
+    print("no")
