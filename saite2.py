cd43b8fbf162b1ae8d9b666a91eab19507bbda39
nandhu321 <48095407+nandhu321@users.noreply.github.com>
2019-07-04 13:47:22 +0530

    Create saite2.py

---
+++ saite2.py
@@ -0,0 +1,13 @@
+m,n=map(int,input().split())
+lis=list(map(int,input().split()))
+l=len(lis)
+count=0
+for i in range(0,l):
+    if(n==lis[i]):
+        count=count+1
+    else:
+        continue
+if(count==0):
+    print("no")
+else:
+    print("yes")
